c22f40994d85d0205550ea329974c43f2bbd7211
Murat Bayram (y509177) <Murat.Bayram@softwareag.com>
2021-03-10 19:54:16 +0100

    reporting live with real data

--- TagesReport/pdfcreator/pdf.py
+++ TagesReport/pdfcreator/pdf.py
@@ -52,14 +52,10 @@ class PDFgenerator:
 		self.positiv = self.content[2]
 		self.negativ = self.content[3]
 		self.unklar = self.content[4]
-		if self.tests > 0:
-			self.rate = self.positiv/self.tests
-		else:
-			self.rate = 0
+
 		# Pie chart, where the slices will be ordered and plotted counter-clockwise:
 		self.labels = 'Positiv', 'Negativ', 'Unklar'
-		self.sizes = [12, 24, 2]
-		#self.sizes = [self.positiv, self.negativ, self.unklar]
+		self.sizes = [self.positiv, self.negativ, self.unklar]
 		self.explode = (0, 0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 		self.fig1, self.ax1 = plt.subplots()
 		self.ax1.pie(self.sizes, explode=self.explode, labels=self.labels, autopct=lambda p: '{:.2f}%  ({:,.0f})'.format(p, p * sum(self.sizes)/100),
