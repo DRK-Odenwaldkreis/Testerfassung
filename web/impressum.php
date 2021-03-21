d0f05732721b6e2dc4e95b06342172f2e261dd45
mbay-SAG <58465677+mbay-SAG@users.noreply.github.com>
2021-03-21 08:38:53 +0100

    Update impressum.php
    added contact data to official impressum

--- web/impressum.php
+++ web/impressum.php
@@ -33,6 +33,17 @@ echo $GLOBALS['G_html_main_right_a'];
 echo '<h3>Impressum und Kontakt</h3>
 <a href="https://drk-odenwaldkreis.de/impressum/">Direkt beim DRK Kreisverband Odenwaldkreis e. V.</a>';
 
+echo '<h3>Rückfragen</h3>
+<p>Bei Fragen zum Testergebnis oder organisatorischen Anfragen bitte eine E-Mail schreiben an:</p>
+<p><a href="mailto:testzentrum@drk-odenwaldkreis.de">testzentrum@drk-odenwaldkreis.de</a></p>
+<p>&nbsp;</p>';
+
+echo '<h3>Technischer Support für die Teams vor Ort</h3>
+<p><a target="_blank" href="https://github.com/DRK-Odenwaldkreis/Testerfassung">Für die Dokumentation der Web-Anwendung hier klicken</a></p>
+<p>&nbsp;</p>
+<p>Bei technischen Fragen zum Zeiterfassungssystem bitte eine E-Mail schreiben an den Support:</p>
+<p><a href="mailto:info@testzentrum-odenwald.">info@testzentrum-odenwald.</a></p>
+<p>&nbsp;</p>';
 
 
 
