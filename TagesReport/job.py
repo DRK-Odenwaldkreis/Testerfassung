a12731a294d4b918305ce2be31e168ba6ffa42e8
Your Name <you@example.com>
2021-03-28 11:33:43 +0200

    log file for debugging

--- TagesReport/job.py
+++ TagesReport/job.py
@@ -13,7 +13,7 @@ from utils.getRequesterMail import get_Leitung_from_StationID
 
 
 logFile = '../../Logs/TagesreportJob.log'
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(filename=logFile,level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('Tagesreport')
 logger.debug('Starting')
