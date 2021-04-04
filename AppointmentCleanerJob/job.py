71ceaafd6f00e327144a14756e9c3b43d6740247
Your Name <you@example.com>
2021-04-04 14:17:38 +0200

    Appointment Cancleation now deletes everything and reopens time slots

--- AppointmentCleanerJob/job.py
+++ AppointmentCleanerJob/job.py
@@ -16,15 +16,32 @@ import datetime
 logFile = '../../Logs/clean.log'
 logging.basicConfig(filename=logFile,level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger('Cleaning for nit verified appointments started on: %s'%(datetime.datetime.now()))
+logger = logging.getLogger('Cleaning for not verified appointments started on: %s'%(datetime.datetime.now()))
 logger.debug('Starting')
 
 if __name__ == "__main__":
     try:
         DatabaseConnect = Database()
-        sql = "Delete FROM Voranmeldung_Verif WHERE Updated < (NOW() - INTERVAL 10 MINUTE);"
-        logger.debug('Deleting all unverified appointments using the following query: %s' % (sql))
-        DatabaseConnect.delete(sql)
+        sql = "Select Voranmeldung_Verif.id, Voranmeldung.id, Voranmeldung.Termin_id FROM Voranmeldung_Verif JOIN Voranmeldung ON Voranmeldung_Verif.id_preregistration = Voranmeldung.id  WHERE Voranmeldung_Verif.updated < (NOW() - INTERVAL 15 MINUTE);"
+        logger.debug('Finding all unverified appointments using the following query: %s' % (sql))
+        unverified = DatabaseConnect.read_all(sql)
+        print(unverified)
+        for i in unverified:
+            try:
+                termine_id = i[2]
+                sql = "Update Termine SET Used = NULL where id=%s;"%(termine_id)
+                logger.debug('Finding used Termin setting back to NULL using the following query: %s' % (sql))
+                DatabaseConnect.update(sql)
+                voranmeldung_id = i[1]
+                sql = "Delete from Voranmeldung where id=%s;"%(voranmeldung_id)
+                logger.debug('Deleting Voranmeldung using the following query: %s' % (sql))
+                DatabaseConnect.delete(sql)
+                verif_id=i[0]
+                sql = "Delete from Voranmeldung_Verif where id=%s;"%(verif_id)
+                logger.debug('Deleting Verif entry using the following query: %s' % (sql))
+                DatabaseConnect.delete(sql)
+            except Exception as e:
+                logging.error("The following error occured: %s" % (e))
         logger.debug('Done for all')
     except Exception as e:
         logging.error("The following error occured: %s" % (e))
