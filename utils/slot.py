e03baebe44c03a0ea48b245963d518160dcf4462
Your Name <you@example.com>
2021-04-09 08:21:45 +0200

    bug in slot time calculation

--- utils/slot.py
+++ utils/slot.py
@@ -11,17 +11,27 @@ def get_slot_time(slot, stunde):
     if slot == 1:
         start = '00'
         ende = '15'
+        stundeStart = stunde
+        stundeEnd = stunde
     elif slot == 2:
         start = '15'
         ende = '30'
+        stundeStart = stunde
+        stundeEnd = stunde
     elif slot == 3:
         start = '30'
         ende = '45'
+        stundeStart = stunde
+        stundeEnd = stunde
     elif slot == 4:
         start = '45'
         ende = '00'
+        stundeStart = stunde
+        stundeEnd = stunde + 1
     else:
         start ='00'
         ende = '00'
-    appointment = "%s:%s - %s:%s" % (str(stunde),str(start),str(stunde),str(ende))
+        stundeStart = stunde
+        stundeEnd = stunde
+    appointment = "%s:%s - %s:%s" % (str(stundeStart),str(start),str(stundeEnd),str(ende))
     return appointment
