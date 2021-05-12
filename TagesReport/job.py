5fd009fee5e58fc8d527aaa6e0870cf8f0c6c036
mbay <murat.bayram@softwareag.com>
2021-05-12 23:47:51 +0200

    corrections tagesreport

--- TagesReport/job.py
+++ TagesReport/job.py
@@ -13,7 +13,7 @@ from utils.getRequesterMail import get_Leitung_from_StationID
 
 
 logFile = '../../Logs/TagesreportJob.log'
-logging.basicConfig(filename=logFile,level=logging.DEBUG,
+logging.basicConfig(filename=logFile,level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('Tagesreport')
 logger.info('Starting Tagesreporting')
@@ -27,6 +27,10 @@ def create_PDFs(content, date, station):
     times = []
     age = []
     children = 0
+    CWA_anonym = 0
+    CWA_named = 0
+    poc_reg = 0
+    pre_reg = 0
     for i in content:
         times.append(i[4].total_seconds()/60)
         today = datetime.date.today()
@@ -43,12 +47,24 @@ def create_PDFs(content, date, station):
             unklar += 1
         else:
             pass
+        if i[6] == "POCREG":
+            poc_reg += 1
+        elif i[6] == "PREREG":
+            pre_reg += 1
+        else:
+            pass
+        if i[7] == 1:
+            CWA_named += 1
+        elif i[7] == 2:
+            CWA_anonym += 1
+        else:
+            pass
     logger.debug('Positive tests: %s' % (str(positiv)))
     logger.debug('Negative tests: %s' % (str(negativ)))
     logger.debug('Unclear tests: %s' % (str(unklar)))
     tests = unklar + negativ + positiv
     logger.debug('Calculated this total number of tests: %s' % (str(tests)))
-    pdfcontent = [station, tests, positiv, negativ, unklar, times, age, children]
+    pdfcontent = [station, tests, positiv, negativ, unklar, times, age, children, CWA_anonym, CWA_named, pre_reg, poc_reg]
     PDF = PDFgenerator(pdfcontent, f"{date}")
     return PDF.generate()
 
@@ -67,7 +83,7 @@ if __name__ == "__main__":
         sql = "Select Vorgang.Teststation, Station.Ort, Station.Adresse from Vorgang JOIN Station ON Vorgang.Teststation = Station.id where Ergebniszeitpunkt Between '%s 00:00:00' and '%s 23:59:59' GROUP BY Vorgang.Teststation;" % (requestedDate.replace('-', '.'), requestedDate.replace('-', '.'))
         teststationen = DatabaseConnect.read_all(sql)
         for station in teststationen:
-            sql = "Select id,Ergebnis,Ergebniszeitpunkt,Teststation,TIMEDIFF(Ergebniszeitpunkt,Registrierungszeitpunkt),Geburtsdatum from Vorgang where Teststation = %s and Ergebniszeitpunkt Between '%s 00:00:00' and '%s 23:59:59';" % (station[0],
+            sql = "Select id,Ergebnis,Ergebniszeitpunkt,Teststation,TIMEDIFF(Ergebniszeitpunkt,Registrierungszeitpunkt),Geburtsdatum,reg_type,CWA_request from Vorgang where Teststation = %s and Ergebniszeitpunkt Between '%s 00:00:00' and '%s 23:59:59';" % (station[0],
             requestedDate.replace('-', '.'), requestedDate.replace('-', '.'))
             logger.debug('Getting all Events for a date with the following query: %s' % (sql))
             exportEvents = DatabaseConnect.read_all(sql)
@@ -76,6 +92,14 @@ if __name__ == "__main__":
             if send:
                 logger.debug('Sending Mail')
                 send_mail_report(filename,requestedDate,get_Leitung_from_StationID(station[0]))
+        sql = "Select Vorgang.id,Ergebnis,Ergebniszeitpunkt,Teststation,TIMEDIFF(Ergebniszeitpunkt,Registrierungszeitpunkt),Geburtsdatum,reg_type,CWA_request from Vorgang JOIN Testtyp ON Vorgang.Testtyp_id=Testtyp.id where Ergebniszeitpunkt Between '%s 00:00:00' and '%s 23:59:59' and Testtyp.IsPCR=0;" % (requestedDate.replace('-', '.'), requestedDate.replace('-', '.'))
+        logger.debug('Getting all Events for a date with the following query: %s' % (sql))
+        exportEvents = DatabaseConnect.read_all(sql)
+        logger.debug('Received the all entries: %s' %(str(exportEvents)))
+        filename = create_PDFs(exportEvents, requestedDate, ["Gesamt","Alle Testzentren","(ohne PCR)"])
+        if send:
+                logger.debug('Sending Mail')
+                send_mail_report(filename,requestedDate,get_Leitung_from_StationID(0))
         logger.info('Done')
     except Exception as e:
         logging.error("The following error occured: %s" % (e))
