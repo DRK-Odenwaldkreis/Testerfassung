1b1a49004cec8a465f83666399ef342320610947
mbay <murat.bayram@softwareag.com>
2022-01-27 11:17:26 +0100

    changing debug level

--- DCCJob/job.py
+++ DCCJob/job.py
@@ -320,7 +320,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger('DCC Handler: %s' %(datetime.now()))
     logger.info('Starting DCC Request')
